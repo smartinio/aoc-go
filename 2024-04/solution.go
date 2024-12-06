60262110098ee5d8121cc630e8f6cadbb3569212
Sam Martin <sam.martin@wolt.com>
2024-12-06 14:01:10 +0100

    Simplify 2024-04

--- 2024-04/solution.go
+++ 2024-04/solution.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-	"bufio"
+	_ "embed"
 	"fmt"
-	"os"
+	"strings"
 )
 
+//go:embed input.txt
+var input string
+var w int = strings.Index(input, "\n")
+var h int = strings.Count(input, "\n")
+
 func main() {
-	lines := parseInput()
 	part1, part2 := 0, 0
 
-	for y, line := range lines {
-		for x, char := range line {
+	for x := range w {
+		for y := range h {
+			char := charAt(x, y)
+
 			if char == 'X' {
-				part1 += countMASFrom(x, y, lines)
+				part1 += countMASFrom(x, y)
 			}
 
-			if char == 'A' && isBetweenMultipleMS(x, y, lines) {
+			if char == 'A' && isBetweenMultipleMS(x, y) {
 				part2 += 1
 			}
 		}
 	}
 
-	fmt.Println("xmas count:", part1)
-	fmt.Println("x-mas count:", part2)
+	fmt.Println("part1:", part1)
+	fmt.Println("part2:", part2)
 }
 
-func countMASFrom(x int, y int, lines []string) int {
+func charAt(x int, y int) rune {
+	if x >= w || y >= h || x < 0 || y < 0 {
+		return 0
+	}
+
+	return rune(input[int(y)*int(w+1)+int(x)])
+}
+
+func countMASFrom(x int, y int) int {
 	count := 0
 	dirs := []int{0, 1, -1}
-	mas := "MAS"
-
-	dirSpellsMAS := func(dx int, dy int) bool {
-		for i, char := range mas {
-			k := i + 1
-			dxx, dyy := x+k*dx, y+k*dy
-
-			if dyy < 0 || dxx < 0 || dyy > len(lines)-1 || dxx > len(lines[y])-1 || lines[dyy][dxx] != byte(char) {
-				return false
-			}
-		}
-
-		return true
-	}
 
 	for _, dx := range dirs {
 		for _, dy := range dirs {
-			if (dx != 0 || dy != 0) && dirSpellsMAS(dx, dy) {
+			if !(dx == 0 && dy == 0) && dirSpells(x, y, dx, dy, "MAS") {
 				count += 1
 			}
 		}
@@ -55,17 +55,13 @@ func countMASFrom(x int, y int, lines []string) int {
 	return count
 }
 
-func isBetweenMultipleMS(x int, y int, lines []string) bool {
-	if x == 0 || x == len(lines[y])-1 || y == 0 || y == len(lines)-1 {
-		return false
-	}
-
+func isBetweenMultipleMS(x int, y int) bool {
 	dirs := []int{1, -1}
 	match := false
 
 	for _, dx := range dirs {
 		for _, dy := range dirs {
-			if lines[y+dy][x+dx] == 'M' && lines[y-dy][x-dx] == 'S' {
+			if dirSpells(x, y, dx, dy, "M") && dirSpells(x, y, -dx, -dy, "S") {
 				if match {
 					return true
 				}
@@ -77,17 +73,12 @@ func isBetweenMultipleMS(x int, y int, lines []string) bool {
 	return false
 }
 
-func parseInput() []string {
-	file, _ := os.Open("2024-04/input.txt")
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	lines := []string{}
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+func dirSpells(x int, y int, dx int, dy int, word string) bool {
+	for i, char := range word {
+		if charAt(x+(i+1)*dx, y+(i+1)*dy) != char {
+			return false
+		}
 	}
 
-	return lines
+	return true
 }
