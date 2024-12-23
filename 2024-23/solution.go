c202717338635858a3ea0862a7adbfe0822b5ced
Sam Martin <sam.martin@wolt.com>
2024-12-23 08:13:50 +0100

    2024-23

---
+++ 2024-23/solution.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	_ "embed"
+	"main/perf"
+	"slices"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func solution() (int, string) {
+	part1, part2 := 0, ""
+
+	conns := map[string][]string{}
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	for _, line := range lines {
+		c := strings.Split(line, "-")
+		conns[c[0]] = append(conns[c[0]], c[1])
+		conns[c[1]] = append(conns[c[1]], c[0])
+	}
+
+	// part 1
+	{
+		type Key struct{ a, b, c string }
+		res := [][]string{}
+		seen := map[Key]bool{}
+
+		for k, v := range conns {
+			for i := range v {
+				for j := range v[1:] {
+					a, b := v[i], v[j]
+
+					if k != "t" && a[0] != 't' && b[0] != 't' {
+						continue
+					}
+
+					n := []string{k, a, b}
+					slices.Sort(n)
+					key := Key{n[0], n[1], n[2]}
+
+					if seen[key] {
+						continue
+					}
+
+					seen[key] = true
+
+					if slices.Contains(conns[a], b) {
+						res = append(res, []string{k, a, b})
+					}
+				}
+			}
+		}
+
+		part1 = len(res)
+	}
+
+	// part 2
+	{
+		maxScore, code := 0, ""
+
+		for k, friends := range conns {
+			mutuals := map[string]int{}
+
+			for _, friend := range friends {
+				for _, other := range conns[friend] {
+					if slices.Contains(friends, other) {
+						mutuals[friend]++
+					}
+				}
+			}
+
+			score := 1
+
+			for _, v := range mutuals {
+				score *= v
+			}
+
+			if score > maxScore {
+				maxScore = score
+				parts := []string{k}
+				for k := range mutuals {
+					parts = append(parts, k)
+				}
+				slices.Sort(parts)
+				code = strings.Join(parts, ",")
+			}
+		}
+
+		part2 = code
+	}
+
+	return part1, part2
+}
+
+func main() {
+	perf.Bench(20, solution)
+}
