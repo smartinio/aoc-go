9d7385d86dc59f8450051a877153580149a5e667
Sam Martin <me@smartin.io>
2025-12-05 23:30:11 +0100

    2025/02 - improve perf 100x

--- 2025/02/solution.go
+++ 2025/02/solution.go
@@ -12,49 +12,50 @@ var input string
 
 func solution() (int, int) {
 	part1, part2 := 0, 0
-
 	lines := strings.Split(strings.TrimSpace(input), ",")
 
 	for _, line := range lines {
+		counted := make(map[int]bool)
 		nums := strings.Split(line, "-")
-		n1, _ := strconv.Atoi(nums[0])
-		n2, _ := strconv.Atoi(nums[1])
+		start, end := nums[0], nums[1]
+		n1, _ := strconv.Atoi(start)
+		n2, _ := strconv.Atoi(end)
 
-	idScan:
-		for i := n1; i <= n2; i++ {
-			num := strconv.Itoa(i)
-			length := len(num)
+		for times := 2; times <= max(len(start), len(end)); times++ {
+			result := sumInvalid(start, times, n1, n2, counted)
 
-			if length%2 == 0 {
-				halfA := num[0 : length/2]
-				halfB := num[length/2:]
-
-				if halfA == halfB {
-					part1 += i
-				}
+			if times == 2 {
+				part1 += result
 			}
 
-		segmentScan:
-			for k := 1; k <= length/2; k++ {
-				slice := num[0:k]
-
-				for m := k; m <= length-k; m += k {
-					other := num[m : m+k]
-
-					if slice != other {
-						continue segmentScan
-					}
-				}
-
-				if length%k == 0 {
-					part2 += i
-					continue idScan
-				}
-			}
+			part2 += result
 		}
 	}
 
 	return part1, part2
+}
+
+func sumInvalid(start string, times, n1, n2 int, counted map[int]bool) int {
+	sum := 0
+	chunk := start[0 : len(start)/times]
+
+	for {
+		invalid, _ := strconv.Atoi(strings.Repeat(chunk, times))
+
+		if invalid > n2 {
+			break
+		}
+
+		if invalid >= n1 && !counted[invalid] {
+			counted[invalid] = true
+			sum += invalid
+		}
+
+		next, _ := strconv.Atoi(chunk)
+		chunk = strconv.Itoa(next + 1)
+	}
+
+	return sum
 }
 
 func main() {
